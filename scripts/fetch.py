d7a5b5e141922c416505c4c06c7a74b49451fddc
Revant Kumar <revantkumar@lawn-128-61-47-88.lawn.gatech.edu>
2014-09-20 07:36:46 -0400

    Updating fetch.py

--- scripts/fetch.py
+++ scripts/fetch.py
@@ -1,13 +1,34 @@
 import urllib2
 import json
 
-response = urllib2.urlopen("http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.12.0&SECURITY-APPNAME=GeorgiaI-927c-4229-856a-e1ec1717d0b9&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords='pre-CBS'%22&outputSelector(0)=SellerInfo&outputSelector(1)=StoreInfo")
-data = json.load(response)   
+def findItemsByKeywords(keyword):
+	url = """http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&
+																					SERVICE-VERSION=1.12.0&
+																					SECURITY-APPNAME=GeorgiaI-927c-4229-856a-e1ec1717d0b9&
+																					RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&
+																					outputSelector(0)=SellerInfo&
+																					outputSelector(1)=StoreInfo&
+																					keywords=""" + keyword
+	response = urllib2.urlopen(url)
+	data = json.load(response)
+	count = int(data["findItemsByKeywordsResponse"][0]['searchResult'][0]['@count'])
+	return data, count
 
-#Count of the item searched by that keyword
-count = int(data["findItemsByKeywordsResponse"][0]['searchResult'][0]['@count'])
+def getUserInfo(username):
+	url = """http://open.api.ebay.com/shopping?callname=GetUserProfile&
+												responseencoding=JSON&
+												appid=GeorgiaI-927c-4229-856a-e1ec1717d0b9&siteid=0
+												&version=525&
+												UserID=""" + username
+	response = urllib2.urlopen(url)
+	data = json.load(response)
+	userdata = data["user"]
+	return userdata
 
-#Seller Info for each of the item. This may be repated
-for i in range(count):
-	print data["findItemsByKeywordsResponse"][0]['searchResult'][0]['item'][i]['sellerInfo']
-	print "\n"
+def findUser(keyword):
+	data, count = findItemsByKeywords(keyword)
+	userlocation = {};
+	for i in range(count):
+		username = data["findItemsByKeywordsResponse"][0]['searchResult'][0]['item'][i]['sellerInfo'][0]['sellerUserName'][0]
+		userdata = getUserInfo(username)
+		location = data["findItemsByKeywordsResponse"][0]['searchResult'][0]['item'][i]['location'][0]
